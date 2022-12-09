f1e2088a0f2df8e6b7e52aee1e9ade1eabd689a9
@rajat1106 <rajatharne@gmail.com>
2022-12-10 03:05:39 +0530

    imgae improved

--- src/pages/myOrder/[donationId].tsx
+++ src/pages/myOrder/[donationId].tsx
@@ -5,6 +5,7 @@ import { OrderStatus } from "@prisma/client";
 import enumToArray from "@utils/enumToArray";
 import { trpc } from "@utils/trpc";
 import { useRouter } from "next/router";
+import Image from "next/image";
 
 interface OrderStatusUIArr {
   value: OrderStatus | undefined;
@@ -114,7 +115,7 @@ const MyOrder = () => {
                   </div>
 
                   <div className="flex-shrink-0 w-10 h-10 rounded-full bg-green-500 inline-flex items-center justify-center text-white relative z-10">
-                    <img src="user.png" width="25" height="30" />
+                    <Image src="/user.png" width="25" height="30" alt={"user"}/>
                     <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
                     <circle cx="12" cy="7" r="4"></circle>
                   </div>
@@ -128,7 +129,7 @@ const MyOrder = () => {
                     <div className="h-full w-1 bg-green-200 pointer-events-none"></div>
                   </div>
                   <div className="flex-shrink-0 w-10 h-10 rounded-full bg-green-500 inline-flex items-center justify-center text-white relative z-10">
-                    <img src="scooter.png" width="30" height="40" />
+                    <Image src="/scooter.png" width="30" height="40" alt={"scooter"}/>
                     <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
                   </div>
                   <div className="flex-grow pl-4">
@@ -142,7 +143,7 @@ const MyOrder = () => {
                     <div className="h-full w-1 bg-green-200 pointer-events-none"></div>
                   </div>
                   <div className="flex-shrink-0 w-10 h-10 rounded-full bg-green-500 inline-flex items-center justify-center text-white relative z-10">
-                    <img src="placeholder.png" width="25" height="30" />
+                    <Image src="/placeholder.png" width="25" height="30" alt={"placeholder"}/>
                     <circle cx="12" cy="5" r="3"></circle>
                     <path d="M12 22V8M5 12H2a10 10 0 0020 0h-3"></path>
                   </div>
@@ -156,7 +157,7 @@ const MyOrder = () => {
                     <div className="h-full w-1 bg-green-200 pointer-events-none"></div>
                   </div>
                   <div className="flex-shrink-0 w-10 h-10 rounded-full bg-green-500 inline-flex items-center justify-center text-white relative z-10">
-                    <img src="delivery-time.png" width="25" height="30" />
+                    <Image src="delivery-time.png" width="25" height="30" alt={"delivery-time"}/>
                     <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
                   </div>
                   <div className="flex-grow pl-4">
@@ -166,7 +167,7 @@ const MyOrder = () => {
 
                 <div className="flex relative">
                   <div className="flex-shrink-0 w-10 h-10 rounded-full bg-green-500 inline-flex items-center justify-center text-white relative z-10">
-                    <img src="check-mark.png" width="25" height="30" />
+                    <Image src="check-mark.png" width="25" height="30" alt={"check-mark"}/>
                     <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
                     <path d="M22 4L12 14.01l-3-3"></path>
                   </div>
@@ -179,7 +180,7 @@ const MyOrder = () => {
             </div>
           </div>
         </section>
-        /*----------------------------------- */
+/*--------------------------------------------------------------------------- */
         <Footer />
       </div>
     </>
